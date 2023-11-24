b652595512253854610aadf30f12bf1421b97b88
tiangenglu <58899759+tiangenglu@users.noreply.github.com>
2023-11-24 17:06:53 -0500

    add while not conditions to avoid all zeros

--- 11212023_grouped_median.py
+++ 11212023_grouped_median.py
@@ -36,14 +36,16 @@ def grouped_median(df, sorted_class, var, I = 1):
 
     Returns a single-row data frame
     -------
-    None.
+    Prerequisite: (1) the var column must be numeric. (2) the var column cannot be all zeros.
 
     """
     df = df.sort_values(sorted_class, ascending = True).reset_index(drop = True)
     df['cum'] = np.cumsum(df[var])
-    L = df[sorted_class][df['cum'] >= df[var].sum() / 2].min
-    CF = df['cum'][df[sorted_class] == L - 1].iloc[0]
-    F = df[var][df[sorted_class] == L].iloc[0]
-    N = df[var].sum()
-    grouped_median = L + I * (0.5 * N - CF) / F
-    return pd.DataFrame.from_dict([{'L': L, 'CF': CF, 'F': F, 'N': N, 'median': grouped_median}])
+    while not df[var].var() == 0 & df[var].sum() == 0:
+        L = df[sorted_class][df['cum'] >= df[var].sum() / 2].min
+        CF = df['cum'][df[sorted_class] == L - 1].iloc[0]
+        F = df[var][df[sorted_class] == L].iloc[0]
+        N = df[var].sum()
+        grouped_median = L + I * (0.5 * N - CF) / F
+        output_df = pd.DataFrame.from_dict([{'L': L, 'CF': CF, 'F': F, 'N': N, 'median': grouped_median}])
+    return output_df
